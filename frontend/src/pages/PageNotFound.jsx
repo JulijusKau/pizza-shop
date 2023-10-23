69e252cd365a308d2e0efabe237312dd739edffe
JulijusK <asdabezda@gmail.com>
2023-10-23 16:25:59 +0300

    page not found created

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,7 +1,41 @@
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
 export const PageNotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div></div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        fontFamily: "var(--mainFont)",
+        color: "var(--colorF)",
+        fontSize: "3rem",
+      }}
+    >
+      WHOOPS, PAGE NOT FOUND. 404
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "var(--colorD)",
+          fontSize: "1.7rem",
+          "&:hover": {
+            backgroundColor: "var(--colorF)",
+          },
+        }}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        BACK HOME
+      </Button>
     </div>
   );
 };
